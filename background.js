3d83edc65781b4ed584fc02e98ebb9b38cba107d
Hajime Fukuda <hajime.fukuda@ipmu.jp>
2017-08-17 14:58:20 +0900

    forgot to add

--- background.js
+++ background.js
@@ -4,11 +4,19 @@ let latestRequestId = null;
 chrome.webRequest.onAuthRequired.addListener(details => {
   if (latestRequestId == details.requestId) return;
   latestRequestId = details.requestId;
-  let password = details.realm.split('password: ')[1].substr(0, 6).replace(' ','');
+  let request = new XMLHttpRequest();
+  request.open('GET', 'https://kds.kek.jp/info/Authentication.html', false);
+  request.send(null);
+  //let password = details.realm.split('password: ')[1].substr(0, 6).replace(' ','');
+  if (request.status != 200) return;
+  let passMatch = request.responseText.match(/PASSWORD : (\w+)/);
+
+  if (passMatch.length != 2) return;
+
   return {
     authCredentials: {
       username: 'kds',
-      password: password,
+      password: passMatch[1],
     }
   };
 }, {urls: ["<all_urls>"]}, ["blocking"]);
